59f3db0edfdbd846a9892b6116cdc136f03df4c0
BenZiAv <benavnilov@gmail.com>
2023-05-18 22:09:48 +0300

    ch all pages

---
+++ src/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import Banner from './Banner';
+import Content from './Content';
+import Footer from './Footer';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
