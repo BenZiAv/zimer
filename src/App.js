031aea376e7953ce1f5c1bd513a9c5fa86dab60c
sray85 <y.s.aldin@gmail.com>
2023-05-18 19:05:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const ZimmerCard = ({ name, description, image, price }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React dfghjkl
-        </a>
-      </header>
+    <div className="zimmer-card">
+      <img src={image} alt={name} />
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <p>Price: {price}</p>
     </div>
   );
-}
+};
 
-export default App;
+const ZimmerPage = () => {
+  const zimmers = [
+    {
+      name: 'Zimmer 1',
+      description: 'This is the description of Zimmer 1.',
+      image: 'zimmer1.jpg',
+      price: '$100'
+    },
+    {
+      name: 'Zimmer 2',
+      description: 'This is the description of Zimmer 2.',
+      image: 'zimmer2.jpg',
+      price: '$150'
+    },
+    // Add more zimmers as needed
+  ];
+
+  return (
+    <div className="zimmer-page">
+      <h1>Zimmer Listings</h1>
+      {zimmers.map((zimmer, index) => (
+        <ZimmerCard
+          key={index}
+          name={zimmer.name}
+          description={zimmer.description}
+          image={zimmer.image}
+          price={zimmer.price}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ZimmerPage;
