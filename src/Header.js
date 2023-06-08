b1e8e1a848970483ebf248a4f4b917598a6ac2ae
BenZiAv <benavnilov@gmail.com>
2023-06-08 17:02:22 +0300

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,30 +1,7 @@
 import React from 'react';
 
-const Header = () => {
-  return (
-    <header className="header">
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/services">Services</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <div>
-        <input type="text" placeholder="Search" />
-        <button>Search</button>
-      </div>
-    </header>
-  );
+const Header = (props) => {
+  return <header className={props.className}>תוכן הכותרת</header>;
 };
 
 export default Header;
