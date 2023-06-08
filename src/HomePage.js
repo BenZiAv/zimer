02ea214b7158fa23633ea85325c95843c8c5bd68
BenZiAv <benavnilov@gmail.com>
2023-06-08 18:49:43 +0300

    update

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,16 +3,16 @@ import Header from './Header';
 import Banner from './Banner';
 import Content from './Content';
 import Footer from './Footer';
-import Image from './../src/images/zimer1.jpg';
 import './HomePage.css';
 
 
 
 const HomePage = () => {
   return (
-    <div>
-      <img src="./../src/images/zimer1.jpg" alt=""></img>
-      <Header className="header" />
+    <div className='body1'>
+      
+      
+      {/* <Header className="header" /> */}
       {/* <Banner className="banner" />
       <Content className="content" />
       <Footer className="footer" /> */}
