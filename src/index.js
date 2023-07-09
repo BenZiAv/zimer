7133225aca9b67814b7c0a5795806ec2f1a8c83c
BenZiAv <benavnilov@gmail.com>
2023-07-09 21:34:15 +0300

    Revert "mongoo db connect"
    This reverts commit 29f93608f5f63f831e3512dcdd8d7ee6557abfba.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomePage from './HomePage';
 import reportWebVitals from './reportWebVitals';
-import './db/mongoConnect';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +10,7 @@ root.render(
     <HomePage />
   </React.StrictMode>
 );
-const db = require("./db/mongoConnect");
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
