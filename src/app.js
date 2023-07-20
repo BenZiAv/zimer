a44504257ae351d3f1e21b725c24f5dea2c22db9
sray85 <y.s.aldin@gmail.com>
2023-07-20 19:41:58 +0300

    index.html

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginForm from "./Login";
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h2>Count down to 2023-10-</h2>
+            <h3>xxx</h3>
+        </div>
+    );
+    
+}
+export default App;
