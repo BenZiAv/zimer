203e4129be0da080c12ce8b0e76057eb28a65388
sray85 <y.s.aldin@gmail.com>
2023-07-25 00:03:14 +0300

    dataBase

---
+++ src/loader/loader.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../loader/loader.scss";
+
+const Loader: React.FC = () => {
+  return <div className="loader"></div>;
+};
+
+export default Loader;
---
+++ src/loader/loader1.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../loader/loader.scss";
+
+const Loader1: React.FC = () => {
+  return <div className="loader1"></div>;
+};
+
+export default Loader1;
