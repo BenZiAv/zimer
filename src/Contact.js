073e540ef0591651cc232517ae23218c16169452
sray85 <y.s.aldin@gmail.com>
2023-09-10 19:47:06 +0300

    כג

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import React,{Component} from 'react';
-import { Component } from 'react';
+//import { Component } from 'react';
 
 
 class contact extends Component{
