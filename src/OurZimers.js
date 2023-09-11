ae712be00285cf42c6b5ecc07a61159d9ab05cbc
BenZiAv <benavnilov@gmail.com>
2023-09-11 20:26:26 +0300

    OurZimer component
    removing About.js
    replacing it to OurZimer.js
    4 columns for each unit
    
    Co-Authored-By: sray85 <103896325+sray85@users.noreply.github.com>

---
+++ src/OurZimers.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const OurZimers = ({ activePage }) => {
+  // Sample B&B units data (replace with your actual data)
+  const bnbUnits = [
+    { 
+      id: 1, 
+      name: 'B&B Unit 1', 
+      image: 'https://www.afristay.com/media/thumbnails/pictures/places/257/91190.jpg.1366x768_q85_crop_upscale.jpg', 
+      description: 'Family unit included 2 bedrooms, one living room, full kitchen, jacuzzi, swimming pool, and two shower rooms.' 
+    },
+    { 
+      id: 2, 
+      name: 'B&B Unit 2', 
+      image: '', 
+      description: '' 
+    },
+    { 
+      id: 3, 
+      name: 'B&B Unit 3', 
+      image: '', 
+      description: '' 
+    },
+    { 
+      id: 4, 
+      name: 'B&B Unit 4', 
+      image: '', 
+      description: '' 
+    },
+    // Add more units as needed
+  ];
+
+  return (
+    <div>
+      <h1 className='h1_our_zimers'>OurZimers</h1>
+
+      {bnbUnits.length >= 4 ? (
+        <div>
+          <h2>Available B&B Units:</h2>
+          <div className='units_grid'>
+            {bnbUnits.slice(0, 4).map((unit) => (
+              <div key={unit.id} className='unit_item'>
+                <strong>{unit.name}</strong>
+                {unit.image && <img src={unit.image} alt={unit.name} />}
+                <p>{unit.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <p>No B&B units available.</p>
+      )}
+    </div>
+  );
+};
+
+export default OurZimers;
